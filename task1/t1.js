fdbbd34023cd22f07ab2f9e95bc2fd2189e56aec
sarapappila <sara.pappila@metropolia.fi>
2024-04-02 20:46:33 +0300

    Tehdään tehtävä 1

--- task1/t1.js
+++ task1/t1.js
@@ -1,20 +1,20 @@
 'use strict';
 
-async function getAPI() {
+import { restaurantModal, restaurantRow } from "./components.js";
+import { fetchAPI } from "./utils.js";
+
+const getAPI = async () => {
     try {
-        const response = await fetch('https://10.120.32.94/restaurant/api/v1/restaurants');
-        if (!response.ok) {
-            throw new Error('Failed to fetch data');
-        }
-        const restaurants = await response.json();
-        displayRestaurants(restaurants);
+        const response = await fetchAPI('restaurants');
+        displayRestaurants(response);
     } catch (error) {
         console.error('Error fetching restaurant data:', error);
         alert('Failed to fetch restaurant data. Please try again later.');
     }
-}
+};
 
-async function getMenu(restaurantID) {
+
+const getMenu = async (restaurantID) => {
     try {
         const response = await fetch(`https://10.120.32.94/restaurant/api/v1/restaurants/daily/${restaurantID}/fi`);
         if (!response.ok) {
@@ -28,37 +28,29 @@ async function getMenu(restaurantID) {
     }
 }
 
-function displayRestaurants(restaurants) {
+const displayRestaurants = (restaurants) => {
     const table = document.querySelector('table');
     restaurants.forEach(restaurant => {
-        const row = document.createElement('tr');
+        const row = restaurantRow(restaurant);
 
-        const nameTD = document.createElement('td');
-        nameTD.textContent = restaurant.name;
-
-        nameTD.addEventListener('click', async () => {
-            document.querySelectorAll('td').forEach(item => {
+        row.addEventListener('click', async () => {
+            document.querySelectorAll('tr').forEach(item => {
                 item.classList.remove('highlight');
             });
 
-            nameTD.classList.add('highlight');
+            row.classList.add('highlight');
             const menu = await getMenu(restaurant._id);
-            openModal(restaurant, menu)
+            openModal(restaurant, menu);
         });
-
-        row.appendChild(nameTD);
-
-        const addressTD = document.createElement('td');
-        addressTD.textContent = restaurant.address;
-        row.appendChild(addressTD);
-
         table.appendChild(row);
     });
-}
+};
 
-function openModal(restaurant, menu) {
+const openModal = (restaurant, menu) => {
     const modal = document.querySelector('dialog');
-    const modalContent = document.createElement('div');
+    const modalContent = restaurantModal(restaurant, menu);
+    modal.innerHTML = modalContent;
+    modal.showModal();
 
     const closeButton = document.createElement('button');
     closeButton.textContent = 'Close';
@@ -66,49 +58,7 @@ function openModal(restaurant, menu) {
         modal.close();
     });
 
-    modalContent.appendChild(closeButton);
-
-    const name = document.createElement('p');
-    name.textContent = restaurant.name;
-    modalContent.appendChild(name);
-
-    const address = document.createElement('p');
-    address.textContent = restaurant.address;
-    modalContent.appendChild(address);
-
-    const postal = document.createElement('p');
-    postal.textContent = restaurant.postalCode;
-    modalContent.appendChild(postal);
-
-    const city = document.createElement('p');
-    city.textContent = restaurant.city;
-    modalContent.appendChild(city);
-
-    const phone = document.createElement('p');
-    phone.textContent = restaurant.phone;
-    modalContent.appendChild(phone);
-
-    const company = document.createElement('p');
-    company.textContent = restaurant.company;
-    modalContent.appendChild(company);
-    console.log('Menu:', menu);
-
-    const menuHeading = document.createElement('h3');
-    menuHeading.textContent = 'Menu';
-    modalContent.appendChild(menuHeading);
-
-    const menuList = document.createElement('ul');
-    menu.courses.forEach(course => {
-        const menuItem = document.createElement('li');
-        menuItem.textContent = `${course.name}: ${course.price} ${course.diets}`;
-        menuList.appendChild(menuItem);
-    });
-    modalContent.appendChild(menuList);
-
-    modal.innerHTML = '';
-    modal.appendChild(modalContent);
-
-    modal.showModal();
+    modal.appendChild(closeButton);
 }
 
 getAPI();
---
+++ task1/utils.js
@@ -0,0 +1,18 @@
+'use script';
+
+import { baseUrl } from "./variables.js";
+
+const fetchAPI = async () => {
+    try {
+        const response = await fetch(`${baseUrl}`);
+        if (!response.ok) {
+            throw new Error('Failed to fetch data');
+        }
+        return await response.json();
+    } catch (error) {
+        console.error('Error fetching restaurant data:', error);
+        alert('Failed to fetch restaurant data. Please try again later.');
+    }
+}
+
+export { fetchAPI };
